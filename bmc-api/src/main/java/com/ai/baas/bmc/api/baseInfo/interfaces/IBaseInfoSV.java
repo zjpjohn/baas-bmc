fa9f2f5365db00db42295c748bf95ebf66ff9ea2
gaogang <gaogang@asiainfo.com>
2016-03-25 19:18:26 +0800

    添加接口异常

--- bmc-api/src/main/java/com/ai/baas/bmc/api/baseInfo/interfaces/IBaseInfoSV.java
+++ bmc-api/src/main/java/com/ai/baas/bmc/api/baseInfo/interfaces/IBaseInfoSV.java
@@ -2,8 +2,11 @@ package com.ai.baas.bmc.api.baseInfo.interfaces;
 
 import com.ai.baas.bmc.api.baseInfo.params.BaseCodeInfo;
 import com.ai.baas.bmc.api.baseInfo.params.QueryInfoParams;
+import com.ai.opt.base.exception.BusinessException;
+import com.ai.opt.base.exception.SystemException;
 
 public interface IBaseInfoSV {
 
-	BaseCodeInfo getBaseInfo(QueryInfoParams param);
+	BaseCodeInfo getBaseInfo(QueryInfoParams param) throws BusinessException,
+    SystemException;
 }
