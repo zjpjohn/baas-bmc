3a7861cd40356ff9d188bd9ade2b2227b70379e8
caoyf <caoyf@asiainfo.com>
2016-03-25 14:13:01 +0800

    修改调用远程服务的工具类

--- bmc-core/src/main/java/com/ai/baas/bmc/util/DshmUtil.java
+++ bmc-core/src/main/java/com/ai/baas/bmc/util/DshmUtil.java
@@ -3,58 +3,54 @@ package com.ai.baas.bmc.util;
 import java.io.IOException;
 import java.util.Properties;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.ai.baas.bmc.context.Constants;
 import com.ai.runner.center.dshm.api.dshmprocess.interfaces.IdshmSV;
 import com.ai.runner.center.dshm.api.dshmservice.interfaces.IdshmreadSV;
 
-@Component
 public class DshmUtil {
     private static IdshmreadSV dshmread;
-    
+
     private static Properties prop;
-    
+
     private static IdshmSV aIdshmSV;
-    @Autowired
-    @Qualifier("IdshmSV")
-    private IdshmSV InitIdshmSV;
-    
-    @PostConstruct
-    private void init(){
-        aIdshmSV = InitIdshmSV;
+
+    private static ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+            "dubbo/consumer/dubbo-consumer.xml");
+
+    static {
+        aIdshmSV = context.getBean(IdshmSV.class);
     }
-    
+
     public synchronized static IdshmreadSV getDshmread() {
-        if(prop == null){
+        if (prop == null) {
             prop = new Properties();
             try {
-                prop.load(DshmUtil.class.getClassLoader().getResourceAsStream("context/config.properties"));
+                prop.load(DshmUtil.class.getClassLoader()
+                        .getResourceAsStream("context/config.properties"));
             } catch (IOException e) {
                 e.printStackTrace();
                 LoggerUtil.log.error(e);
             }
         }
         try {
-            dshmread = (IdshmreadSV) ServiceRegiter.registerService(prop.getProperty("dshm.ip"), prop.getProperty("dshm.port"),
-                    Constants.ShmServiceCode.SHM_SERVICE_CODE);
+            dshmread = (IdshmreadSV) ServiceRegiter.registerService(prop.getProperty("dshm.ip"),
+                    prop.getProperty("dshm.port"), Constants.ShmServiceCode.SHM_SERVICE_CODE);
         } catch (Exception e) {
             e.printStackTrace();
             LoggerUtil.log.error(e);
         }
         return dshmread;
     }
-    
-    public static IdshmSV getIdshmSV(){
-//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("dubbo/consumer/dubbo-consumer.xml");
-//        context.registerShutdownHook();
-//        context.start();
-//        IdshmSV aIdshmSV = context.getBean(IdshmSV.class);
-//        IdshmSV aIdshmSV = DubboConsumerFactory.getService("IdshmSV", IdshmSV.class);
+
+    public static IdshmSV getIdshmSV() {
+        // ClassPathXmlApplicationContext context = new
+        // ClassPathXmlApplicationContext("dubbo/consumer/dubbo-consumer.xml");
+        // context.registerShutdownHook();
+        // context.start();
+        // IdshmSV aIdshmSV = context.getBean(IdshmSV.class);
+        // IdshmSV aIdshmSV = DubboConsumerFactory.getService("IdshmSV", IdshmSV.class);
         return aIdshmSV;
     }
 }
