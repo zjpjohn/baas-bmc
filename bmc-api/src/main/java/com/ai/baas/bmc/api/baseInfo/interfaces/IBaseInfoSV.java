f5a64a5713ab0de0539e02b717b7492696cb5709
gaogang <gaogang@asiainfo.com>
2016-03-28 11:06:58 +0800

    添加注释

--- bmc-api/src/main/java/com/ai/baas/bmc/api/baseInfo/interfaces/IBaseInfoSV.java
+++ bmc-api/src/main/java/com/ai/baas/bmc/api/baseInfo/interfaces/IBaseInfoSV.java
@@ -4,9 +4,25 @@ import com.ai.baas.bmc.api.baseInfo.params.BaseCodeInfo;
 import com.ai.baas.bmc.api.baseInfo.params.QueryInfoParams;
 import com.ai.opt.base.exception.BusinessException;
 import com.ai.opt.base.exception.SystemException;
-
+/**
+ * 基础数据编码
+ *
+ * Date: 2016年3月28日 <br>
+ * Copyright (c) 2016 asiainfo.com <br>
+ * @author gaogang
+ */
 public interface IBaseInfoSV {
 
+	/**
+	 * 基础数据编码查询服务
+	 * @param param
+	 * @return
+	 * @throws BusinessException
+	 * @throws SystemException
+	 * @author gaogang
+	 * @ApiDocMethod
+	 * @ApiCode BaaS-00001
+	 */
 	BaseCodeInfo getBaseInfo(QueryInfoParams param) throws BusinessException,
     SystemException;
 }
