0fb79f746a549dd16f73a09a441c7448ad5a3d44
caoyf <caoyf@asiainfo.com>
2016-03-29 15:25:42 +0800

    接口注释

--- bmc-api/src/main/java/com/ai/baas/bmc/api/priceinfo/interfaces/IPriceInfoSV.java
+++ bmc-api/src/main/java/com/ai/baas/bmc/api/priceinfo/interfaces/IPriceInfoSV.java
@@ -8,18 +8,26 @@ import com.ai.opt.base.exception.SystemException;
 
 
 public interface IPriceInfoSV {
-    /**
-     * 资费查询接口
-     * Date: 2016年3月28日 <br>
-     * Copyright (c) 2016 asiainfo.com <br>
-     * 
-     * @author KAI
-     */
-     @interface GetPriceInfo{}
+     /**
+      * 资费查询接口
+      * @param QueryInfoParams
+      * @return PriceCode
+      * @throws BusinessException
+      * @throws SystemException
+      * @author wangkai
+      * @ApiCode BaaS-00002
+      */
+    @interface GetPriceInfo{}
      public PriceCode getPriceInfo(QueryInfoParams record)throws BusinessException,SystemException;
      
      /**
-      * 标准资费更新
+      * 
+      * @param StandardPriceInfoParams
+      * @return BMC-000000成功；其他失败
+      * @throws BusinessException
+      * @throws SystemException
+      * @author caoyf
+      * @ApiCode BaaS-00003
       */
      @interface UpdatePriceInfo{}
      public String updatePriceInfo(StandardPriceInfoParams record)throws BusinessException,SystemException;
