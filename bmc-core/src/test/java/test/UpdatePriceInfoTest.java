07e8b6ccbe8264c3cecbc62b5fe714b7a746783c
wangkai16 <wangkai16@asiainfo.com>
2016-04-19 19:23:49 +0800

    资费新增通过serviceType subsServiceType判断

--- bmc-core/src/test/java/test/UpdatePriceInfoTest.java
+++ bmc-core/src/test/java/test/UpdatePriceInfoTest.java
@@ -22,23 +22,23 @@ public class UpdatePriceInfoTest {
     @Test
     public void test(){
         StandardPriceInfoParams param = new StandardPriceInfoParams();
-//        param.setTenantId("test");
-//        param.setTradeSeq("11266666");
-//        param.setPriceName("testPriceName");
-//        param.setServiceType("testServiceType");
-//        param.setUsageList(new ArrayList<StanderdPriceInfoUsage>());
-//        StanderdPriceInfoUsage usage = new StanderdPriceInfoUsage();
-//        usage.setAmount(111);
-//        usage.setSubServiceType("testServiceType");
-//        usage.setUnit("分钟");
-//        param.getUsageList().add(usage);
-//        param.setPrice(4.5);
-//        param.setUpdateId("CREATE");
-//        param.setComments("本机自测用");
-        param =  JSONObject.parseObject(
-                "{\"serviceType\":\"VOICE\",\"comments\":\"\",\"tenantPwd\":\"\",\"cycleId\":\"\",\"priceType\":\"\",\"standardId\":\"163\",\"cycleAmount\":0,\"cycleType\":\"\",\"updateId\":\"UPDATE\",\"price\":123,\"tenantId\":\"7BAF6267AE2F421FA8D1E305EE35C4BA\",\"priceName\":\"test02\",\"tradeSeq\":\"7BAF6267AE2F421FA8D1E305EE35C4BA2016041815213185734d95453-fb20-4a12-a395-8cbda644a262\",\"status\":\"NOTEFFECT\",\"usageList\":[{\"amount\":1,\"subServiceType\":\"CALL_OUT\",\"unit\":\"MIN\"}]}"
-                , StandardPriceInfoParams.class);
+        param.setTenantId("test");
+        param.setTradeSeq("11266666");
+        param.setPriceName("testPriceName");
+        param.setServiceType("testServiceType");
+        param.setUsageList(new ArrayList<StanderdPriceInfoUsage>());
+        StanderdPriceInfoUsage usage = new StanderdPriceInfoUsage();
+        usage.setAmount(111);
+        usage.setSubServiceType("testServiceType");
+        usage.setUnit("分钟");
+        param.getUsageList().add(usage);
+        param.setPrice(4.5);
+        param.setUpdateId("CREATE");
+        param.setComments("本机自测用");
+//        param =  JSONObject.parseObject(
+//                "{\"serviceType\":\"VOICE\",\"comments\":\"\",\"tenantPwd\":\"\",\"cycleId\":\"\",\"priceType\":\"\",\"standardId\":\"163\",\"cycleAmount\":0,\"cycleType\":\"\",\"updateId\":\"UPDATE\",\"price\":123,\"tenantId\":\"7BAF6267AE2F421FA8D1E305EE35C4BA\",\"priceName\":\"test02\",\"tradeSeq\":\"7BAF6267AE2F421FA8D1E305EE35C4BA2016041815213185734d95453-fb20-4a12-a395-8cbda644a262\",\"status\":\"NOTEFFECT\",\"usageList\":[{\"amount\":1,\"subServiceType\":\"CALL_OUT\",\"unit\":\"MIN\"}]}"
+//                , StandardPriceInfoParams.class);
         System.out.println("param="+com.alibaba.fastjson.JSON.toJSONString(param));
-        System.out.println(aIPriceInfoSV.updatePriceInfo(param));
+        System.out.println(com.alibaba.fastjson.JSON.toJSONString(aIPriceInfoSV.updatePriceInfo(param)));
     }
 }
