cdb37fafdf01b36d84c5fe2c3db089025d2aac54
liangbs <liangbs@outlook.com>
2016-05-23 16:49:30 +0800

    新增原子服务

---
+++ bmc-core/src/main/java/com/ai/baas/bmc/service/atom/impl/BmcRecordFmtAtomImpl.java
@@ -0,0 +1,33 @@
+package com.ai.baas.bmc.service.atom.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ai.baas.bmc.dao.interfaces.BmcRecordFmtMapper;
+import com.ai.baas.bmc.dao.mapper.bo.BmcRecordFmt;
+import com.ai.baas.bmc.dao.mapper.bo.BmcRecordFmtCriteria;
+import com.ai.baas.bmc.service.atom.interfaces.IBmcRecordFmtAtom;
+
+@Component
+public class BmcRecordFmtAtomImpl implements IBmcRecordFmtAtom {
+
+	@Autowired
+	private BmcRecordFmtMapper mapper;
+	
+    @Override
+    public void add(BmcRecordFmt record) {
+        
+        //1.如果存在，就先删除
+        BmcRecordFmtCriteria example = new BmcRecordFmtCriteria();
+        BmcRecordFmtCriteria.Criteria criteria = example.or();
+        criteria.andTenantIdEqualTo(record.getTenantId())
+                .andServiceIdEqualTo(record.getServiceId())
+                .andSourceEqualTo(record.getSource())
+                .andFormatTypeEqualTo((short) 1);//1：Excel导入的，2：手动添加的。
+        mapper.deleteByExample(example);
+        
+        //2.重新插入
+        mapper.insertSelective(record);
+    }
+
+}
---
+++ bmc-core/src/main/java/com/ai/baas/bmc/service/atom/interfaces/IBmcRecordFmtAtom.java
@@ -0,0 +1,13 @@
+package com.ai.baas.bmc.service.atom.interfaces;
+
+import com.ai.baas.bmc.dao.mapper.bo.BmcRecordFmt;
+
+public interface IBmcRecordFmtAtom {
+    
+    /**
+     * 新增数据
+     * @param record
+     * @author liangbs
+     */
+	public void add(BmcRecordFmt record);
+}
