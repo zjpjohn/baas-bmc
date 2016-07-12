3bf4275fbd3609361257c505dd28c00913ca581c
myto2006 <myto2006@126.com>
2016-07-12 14:14:55 +0800

    no message

--- bmc-core/src/test/java/com/ai/opt/baas/bmc/test/Test.java
+++ bmc-core/src/test/java/com/ai/opt/baas/bmc/test/Test.java
@@ -3,17 +3,16 @@ package com.ai.opt.baas.bmc.test;
 import java.lang.reflect.Field;
 import java.util.Map;
 
+import com.ai.baas.bmc.api.queryidinfo.interfaces.IQueryIdInfoSV;
+import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.alibaba.fastjson.JSON;
 
 public class Test {
 
     public static void main(String[] args) {
-        String a = "{\"a\":\"1\",\"b\":\"2\"}";
-        Map object = (Map) JSON.parse(a);
-        for (Field f : object.getClass().getFields()) {
-            System.out.println(f.getName());
-            // System.out.println(f.get);
-        }
+
+        IQueryIdInfoSV sv = DubboConsumerFactory.getService(IQueryIdInfoSV.class);
+        System.out.println(sv);
     }
 
 }
--- bmc-core/src/test/java/com/ai/opt/baas/bmc/test/api/pricemaking/PricemakingSVDubboTest.java
+++ bmc-core/src/test/java/com/ai/opt/baas/bmc/test/api/pricemaking/PricemakingSVDubboTest.java
@@ -1,5 +1,8 @@
 package com.ai.opt.baas.bmc.test.api.pricemaking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +11,13 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.ai.baas.bmc.api.pricemaking.interfaces.IPricemakingSV;
+import com.ai.baas.bmc.api.pricemaking.params.ElementInfo;
+import com.ai.baas.bmc.api.pricemaking.params.OrderTypeInfo;
+import com.ai.baas.bmc.api.pricemaking.params.PriceElementInfo;
 import com.ai.baas.bmc.api.pricemaking.params.PriceElementInfoZX;
 import com.ai.baas.bmc.api.pricemaking.params.PricemakingResponseZX;
+import com.ai.baas.bmc.api.pricemaking.params.ResponseMessage;
+import com.ai.opt.sdk.dubbo.util.DubboConsumerFactory;
 import com.alibaba.fastjson.JSON;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,10 +27,65 @@ public class PricemakingSVDubboTest {
     protected ApplicationContext ctx;
 
     @Test
-    public void queryPriceMakingZX() {
-        IPricemakingSV sv = (IPricemakingSV) ctx.getBean("iPriceMakingSV");
-        PriceElementInfoZX request = new PriceElementInfoZX();
-        PricemakingResponseZX responseMessage = sv.queryPricemakingZX(request);
+    public void queryPriceMaking() {
+        List<ElementInfo> elementInfoList = new ArrayList<ElementInfo>();
+        ElementInfo elementInfo = new ElementInfo();
+        elementInfo.setName("AERA_ID");
+        elementInfo.setValue("HuaBei1");
+        elementInfoList.add(elementInfo);
+        elementInfo = new ElementInfo();
+        elementInfo.setName("CPU_MEM");
+        elementInfo.setValue("2C4G");
+        elementInfoList.add(elementInfo);
+        elementInfo = new ElementInfo();
+        elementInfo.setName("IO_OPTI");
+        elementInfo.setValue("YES");
+        elementInfoList.add(elementInfo);
+        elementInfo = new ElementInfo();
+        elementInfo.setName("OS");
+        elementInfo.setValue("win");
+        elementInfoList.add(elementInfo);
+        elementInfo = new ElementInfo();
+        elementInfo.setName("SERIAL_ID");
+        elementInfo.setValue("1");
+        elementInfoList.add(elementInfo);
+
+        List<OrderTypeInfo> orderTypeList = new ArrayList<OrderTypeInfo>();
+        OrderTypeInfo orderTypeInfo = new OrderTypeInfo();
+        orderTypeInfo.setListId("1");
+        orderTypeInfo.setPriceType("ECS-INSTANCE");
+        orderTypeInfo.setElementInfoList(elementInfoList);
+        orderTypeList.add(orderTypeInfo);
+        orderTypeInfo = new OrderTypeInfo();
+        orderTypeInfo.setListId("2");
+        orderTypeInfo.setPriceType("ECS-DISK");
+        List<ElementInfo> elementInfoList2 = new ArrayList<ElementInfo>();
+        ElementInfo elementInfo2 = new ElementInfo();
+        elementInfo2.setName("AERA_ID");
+        elementInfo2.setValue("HuaNan1");
+        elementInfoList2.add(elementInfo2);
+        elementInfo2 = new ElementInfo();
+        elementInfo2.setName("DISK_TYPE");
+        elementInfo2.setValue("SSD");
+        elementInfoList2.add(elementInfo2);
+        elementInfo2 = new ElementInfo();
+        elementInfo2.setName("USE_TYPE");
+        elementInfo2.setValue("SYSTEM");
+        elementInfoList2.add(elementInfo2);
+        elementInfo2 = new ElementInfo();
+        elementInfo2.setName("AMOUNT");
+        elementInfo2.setValue("50");
+        elementInfoList2.add(elementInfo2);
+        orderTypeInfo.setElementInfoList(elementInfoList2);
+        orderTypeList.add(orderTypeInfo);
+
+        PriceElementInfo request = new PriceElementInfo();
+        request.setTradeSeq("1");
+        request.setTenantId("test");
+        request.setOrderTypeList(orderTypeList);
+
+        IPricemakingSV sv = DubboConsumerFactory.getService(IPricemakingSV.class);
+        ResponseMessage responseMessage = sv.queryPricemaking(request);
         System.out.println(JSON.toJSONString(responseMessage));
     }
 }
